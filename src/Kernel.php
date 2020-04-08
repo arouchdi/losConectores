5707dc037674fd7bc9fd4ad393fccf054957702d
Amine Rouchdi <aminerouchdi92@gmail.com>
2020-04-08 22:56:57 +0200

    Load parameters files

--- src/Kernel.php
+++ src/Kernel.php
@@ -37,6 +37,8 @@ class Kernel extends BaseKernel
         $container->setParameter('container.dumper.inline_factories', true);
         $confDir = $this->getProjectDir().'/config';
 
+        $loader->load($confDir.'/{parameters}/*'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{parameters}/'.$this->environment.'/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{packages}/'.$this->environment.'/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
